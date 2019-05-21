c5030579c3c83422fb77180e40703f3ebf1d9104
Takashi Matsuoka <matsujirushi@live.jp>
2019-05-21 21:51:05 +0900

    1st

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace dps_devicekeygen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Azure IoT DPS Device SAS Key Generator");
+                Console.WriteLine("Usage:");
+                Console.WriteLine("dps-devicekeygen <Group SAS Key> <Device ID>");
+                Console.WriteLine();
+                return;
+            }
+
+            var groupSasKey = args[0];
+            var deviceId = args[1];
+
+            var key = Convert.FromBase64String(groupSasKey);
+            var hmac = new HMACSHA256(key);
+            var deviceSasKey = hmac.ComputeHash(Encoding.UTF8.GetBytes(deviceId));
+
+            Console.WriteLine($"Device ID     :{deviceId}");
+            Console.WriteLine($"Device SAS Key:{Convert.ToBase64String(deviceSasKey)}");
+        }
+    }
+}
